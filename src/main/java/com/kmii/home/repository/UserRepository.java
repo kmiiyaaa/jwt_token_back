7924fed6d4943985947c330ddb397e1379e056e3
kmiiyaaa <ckm1533@naver.com>
2025-10-30 17:23:08 +0900

    수정사항

--- src/main/java/com/kmii/home/repository/UserRepository.java
+++ src/main/java/com/kmii/home/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.kmii.home.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.kmii.home.entity.User;
 
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	Optional<User> findByUsername(String username);
